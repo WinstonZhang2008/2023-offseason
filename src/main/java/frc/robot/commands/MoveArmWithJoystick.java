c305ffe63e0cbb083ba3e530b0204602fa52e68e
Karthikeya Atmakuru <karthikeya.atmakuru@gmail.com>
2023-09-20 11:46:06 -0700

    Did the assignment
    Arm

--- src/main/java/frc/robot/commands/MoveArmWithJoystick.java
+++ src/main/java/frc/robot/commands/MoveArmWithJoystick.java
@@ -1,7 +1,15 @@
+<<<<<<< Updated upstream
+=======
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+>>>>>>> Stashed changes
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+<<<<<<< Updated upstream
 import frc.robot.subsystems.ArmSubsystem;
 
 public class MoveArmWithJoystick extends CommandBase {
@@ -29,6 +37,40 @@ public class MoveArmWithJoystick extends CommandBase {
     
   }
 
+=======
+import frc.robot.subsystems.arm.ArmSubsystem;
+
+public class MoveArmWithJoystick extends CommandBase {
+  /** Creates a new MoveArmWithJoystick. */
+  ArmSubsystem armSubsystem = new ArmSubsystem();
+  XboxController driverJoyStick = new XboxController(1);
+  public MoveArmWithJoystick(ArmSubsystem armSubsystem, XboxController driverJoystick) {
+    this.armSubsystem = armSubsystem;
+    this.driverJoyStick = driverJoystick;
+    addRequirements(armSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.    
+  @Override
+  public void initialize() {
+    armSubsystem.coast();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    armSubsystem.setPower(driverJoyStick.getLeftY());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    armSubsystem.setPower(0);
+  }
+
+  // Returns true when the command should end.
+>>>>>>> Stashed changes
   @Override
   public boolean isFinished() {
     return false;
