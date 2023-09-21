64cd90ee80cfca2bb18d0e4a20339a6c50569373
Winston Zhang <winstonzhang21@gmail.com>
2023-09-21 15:14:37 -0700

    changed stuff for assignment

--- src/main/java/frc/robot/commands/MoveArmWithJoystick.java
+++ src/main/java/frc/robot/commands/MoveArmWithJoystick.java
@@ -1,47 +1,12 @@
-<<<<<<< Updated upstream
-=======
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
->>>>>>> Stashed changes
-package frc.robot.commands;
-
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-<<<<<<< Updated upstream
-import frc.robot.subsystems.ArmSubsystem;
-
-public class MoveArmWithJoystick extends CommandBase {
-  /** Creates a new MoveArmWithJoystick. */
-
-  //initialize your subsystems, controllers
-  XboxController joystick1;
-  public MoveArmWithJoystick(ArmSubsystem armSubsystem, XboxController joystick1) {
-    //add your parameters to the command
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.joystick1 = joystick1;
-  }
-
-  @Override
-  public void initialize() {}
-
-  @Override
-  public void execute() {
-    double joystickArmPower = joystick1.getLeftY(); //use this joystick armpower to set power to your motors
-  }
-
-
-  @Override
-  public void end(boolean interrupted) {
-    
-  }
-
-=======
 import frc.robot.subsystems.arm.ArmSubsystem;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
 public class MoveArmWithJoystick extends CommandBase {
-  /** Creates a new MoveArmWithJoystick. */
   ArmSubsystem armSubsystem = new ArmSubsystem();
   XboxController driverJoyStick = new XboxController(1);
   public MoveArmWithJoystick(ArmSubsystem armSubsystem, XboxController driverJoystick) {
@@ -70,7 +35,6 @@ public class MoveArmWithJoystick extends CommandBase {
   }
 
   // Returns true when the command should end.
->>>>>>> Stashed changes
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,17 +2,41 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class ArmSubsystem extends SubsystemBase {
+  private TalonFX armMotor;
 
   //add motors here
   public ArmSubsystem() {
     //make proper initializations
+    armMotor = new TalonFX(50);
   }
 
   //consider what methods are necessary for the motor to run effectively
   
   @Override
-  public void periodic() {
+  public void setPower(double power) {
+    armMotor.set(ControlMode.PercentOutput,power);
+  }
 
+  public void brake(){
+    armMotor.setNeutralMode(NeutralMode.Brake);
+  }
+  public void coast(){
+    armMotor.setNeutralMode(NeutralMode.Coast);
   }
 }
